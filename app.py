697fa497fe37b45ca42ec472d12598a4a414c5ad
Rodrigo <rodrigorom2000@gmail.com>
2023-06-12 14:00:22 -0400

    api metadata

--- app.py
+++ app.py
@@ -1,96 +1,105 @@
 from flask import Flask, request, url_for, render_template, make_response
 from markupsafe import escape
 import urllib, urllib.request
+import feedparser
+import time
+import logging
 from werkzeug.utils import secure_filename
+import os 
 
-app = Flask(__name__)
+# Create the 'logs' directory if it doesn't exist
+logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
+os.makedirs(logs_dir, exist_ok=True)
 
-@app.route('/')
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    filename=os.path.join(logs_dir, 'logs.log'),
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)                           # define app using flask
+base_url = 'http://export.arxiv.org/api/query?' # base api query url
+wait_time = 3                                   # number of seconds to wait between calls
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    username = request.cookies.get('username')
-    # use cookies.get(key) instead of cookies[key] to not get a
-    # KeyError if the cookie is missing.
-
-    url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1'
-    data = urllib.request.urlopen(url)
-    xml = data.read().decode('utf-8')
-    print(data.read().decode('utf-8'))
-    return xml
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     if request.method == 'POST':
-        return 'do_the_login()'
+        return search()
     else:
-        return 'show_the_login_form()'
+        return render_template('index.html')
 
-@app.route('/user/<username>')
-def profile(username):
-    return f'{escape(username)}\'s profile'
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['search']
+    logger.info(f'Query: {query}')
+    url = f'{base_url}search_query=all:{query}'
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+    try:
+        response = urllib.request.urlopen(url).read()
+        feed = feedparser.parse(response)
+        logger.info('API response received')
+        parse(feed)
+        return render_template('podcast.html', query=query, results=feed)
+    except Exception as e:
+        logger.error(f'API request failed: {e}')
+        return render_template('error.html')
 
-# url_for('static', filename='style.css')
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def parse(feed):
+    logger.info(f'Feed title: {feed.feed.title}')
+    logger.info(f'Feed last updated: {feed.feed.updated}')
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    resp = make_response(render_template('upload.html'))
-    if request.method == 'POST':
-        f = request.files['the_file']
-        f.save('/var/www/uploads/uploaded_file.txt')
-    return resp
+    logger.info(f'totalResults for this query: {feed.feed.opensearch_totalresults}')
+    logger.info(f'itemsPerPage for this query: {feed.feed.opensearch_itemsperpage}')
+    logger.info(f'startIndex for this query: {feed.feed.opensearch_startindex}')
 
-#If you want the file that was named on the client before it was uploaded:
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         file = request.files['the_file']
-#         file.save(f"/var/www/uploads/{secure_filename(file.filename)}")
-def fetch_arxiv_paper(paper_id):
-    url = f'http://export.arxiv.org/api/query?id_list={paper_id}'
-    data = urllib.request.urlopen(url)
-    xml = data.read().decode('utf-8')
-    print(data.read().decode('utf-8'))
-    return xml
+    for entry in feed.entries:
+        logger.info('e-print metadata')
+        logger.info(f'arxiv-id: {entry.id.split("/abs/")[-1]}')
+        logger.info(f'Published: {entry.published}')
+        logger.info(f'Title: {entry.title}')
 
-@app.route('/paper/<int:paper_id>')
-def get_paper(paper_id):
-    paper = fetch_arxiv_paper(paper_id)
-    if paper:
-        return render_template('paper.html', paper=paper)
-    else:
-        return 'Paper not found'
+        author_string = entry.author
+        try:
+            author_string += ' (%s)' % entry.arxiv_affiliation
+        except AttributeError:
+            pass
+        logger.info(f'Last Author: {author_string}')
 
+        try:
+            authors = ', '.join(author.name for author in entry.authors)
+            logger.info(f'Authors: {authors}')
+        except AttributeError:
+            pass
+
+        for link in entry.links:
+            if link.rel == 'alternate':
+                logger.info(f'abs page link: {link.href}')
+            elif link.title == 'pdf':
+                logger.info(f'pdf link: {link.href}')
+
+        try:
+            journal_ref = entry.arxiv_journal_ref
+        except AttributeError:
+            journal_ref = 'No journal ref found'
+        logger.info(f'Journal reference: {journal_ref}')
+
+        try:
+            comment = entry.arxiv_comment
+        except AttributeError:
+            comment = 'No comment found'
+        logger.info(f'Comments: {comment}')
+
+        logger.info(f'Primary Category: {entry.tags[0]["term"]}')
+
+        all_categories = ', '.join(t['term'] for t in entry.tags)
+        logger.info(f'All Categories: {all_categories}')
+
+        logger.info(f'Abstract: {entry.summary}')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
 # flask --app app run --debug
